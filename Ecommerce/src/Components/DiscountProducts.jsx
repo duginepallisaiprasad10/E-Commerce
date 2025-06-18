0a29c407a71b5ac4cd957aa99b6628299eef25a2
saiprasad <duginepallisaiprasad1025@gmail.com>
2025-06-18 14:41:48 +0530

    fifth commit

--- Ecommerce/src/Components/DiscountProducts.jsx
+++ Ecommerce/src/Components/DiscountProducts.jsx
@@ -23,6 +23,45 @@ export default function DiscountProducts() {
       setShowMessage(false);
     }, 2000);
   };
+  return (
+    <div className="my-10 px-4">
+      {showMessage && ( //message
+        <div className="fixed top-5 right-5 bg-green-600 text-white px-4 py-2 rounded shadow-lg z-50">
+          product added to cart!
+        </div>
+      )}
+
+      {/* Heading */}
+      <div className="flex justify-center mb-10">
+        <h1 className="text-4xl font-bold text-gray-800">Big Discounts</h1>
+      </div>
+
+      {/* Products Grid */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8 justify-items-center">
+        {discoutData.map((item) => (
+          <div
+            key={item.id}
+            className="bg-white shadow-lg rounded-xl w-90 overflow-hidden group relative"
+          >
+            {/* Image Section */}
+            <div className="relative">
+              {/* Discount Badge */}
+              <span className="absolute top-3 left-3 bg-blue-800 text-white text-sm px-3 py-1 rounded-full font-medium">
+                {item.discount}% OFF
+              </span>
+
+              {/* Heart Icon (on hover) */}
+              <span className="absolute top-3 right-3 text-gray-400 hover:text-red-600 opacity-0 group-hover:opacity-100 transition">
+                <FaHeart size={20} />
+              </span>
+              <Link to={`/product/${item.id}`}>
+                <img
+                  src={item.imgUrl}
+                  alt={item.productName}
+                  className="w-full h-78 object-contain p-4"
+                />
+              </Link>
+            </div>
 
 
             {/* Product Info */}
